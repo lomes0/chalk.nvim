8540a96b38a5a05318f1551e00772ebb84a82314
eran.sbt0 <eran.sbt0@gmail.com>
2025-10-27 01:23:58 +0200

    bold,italic

--- lua/chalk/groups/treesitter.lua
+++ lua/chalk/groups/treesitter.lua
@@ -20,11 +20,11 @@ function M.setup(colors, opts)
 	local highlights = {
 		-- Annotations - Sophisticated coral tones
 		["@annotation"] = { fg = c.azure_1 },
-		["@annotation.builtin"] = { fg = c.azure_1 },
+		["@annotation.builtin"] = { fg = c.azure_1, bold = true }, -- Bold for built-in annotations (semantic emphasis)
 
 		-- Attributes - Consistent coral family
 		["@attribute"] = { fg = c.azure_1 },
-		["@attribute.builtin"] = { fg = c.azure_1 },
+		["@attribute.builtin"] = { fg = c.azure_1, bold = true }, -- Bold for built-in attributes (semantic emphasis)
 
 		-- Booleans - Sophisticated ruby distinction
 		["@boolean"] = { fg = c.azure_1 },
@@ -43,33 +43,33 @@ function M.setup(colors, opts)
 
 		-- Constants - Sophisticated golden hierarchy
 		["@constant"] = { fg = c.coral_2 },
-		["@constant.builtin"] = { fg = c.copper_2 },
-		["@constant.macro"] = { fg = c.azure_1 },
+		["@constant.builtin"] = { fg = c.copper_2, bold = true }, -- Bold for built-in constants (semantic emphasis)
+		["@constant.macro"] = { fg = c.azure_1, italic = true }, -- Italic for macro constants (semantic emphasis)
 
 		-- Functions - Enhanced azure visibility
 		["@function"] = { fg = c.turquoise_2 },
-		["@function.builtin"] = { fg = c.azure_1 },
+		["@function.builtin"] = { fg = c.azure_1, bold = true }, -- Bold for built-in functions (semantic emphasis)
 		["@function.call"] = { fg = c.blue },
-		["@function.macro"] = { fg = c.azure_1 },
+		["@function.macro"] = { fg = c.azure_1, bold = true }, -- Bold for macro functions (semantic emphasis)
 		["@function.method"] = { fg = c.azure_1 },
 		["@function.method.call"] = { fg = c.azure_1 },
 
 		-- Keywords - Pastel softness (only _1 and _2 shades)
 		["@keyword"] = { fg = c.mauve_3 },
 		["@keyword.type"] = { fg = c.turquoise_2 },
-		["@keyword.conditional"] = { fg = c.violet_1 },
+		["@keyword.conditional"] = { fg = c.violet_1, bold = true }, -- Bold for conditional keywords (semantic emphasis)
 		["@keyword.conditional.ternary"] = { fg = c.azure_1 },
-		["@keyword.coroutine"] = { fg = c.azure_1, italic = true },
-		["@keyword.debug"] = { fg = c.azure_1 },
+		["@keyword.coroutine"] = { fg = c.azure_1, italic = true }, -- Italic for coroutine keywords (semantic emphasis)
+		["@keyword.debug"] = { fg = c.azure_1, bold = true }, -- Bold for debug keywords (semantic emphasis)
 		["@keyword.directive"] = { fg = c.azure_1 },
 		["@keyword.directive.define"] = { fg = c.azure_1 },
-		["@keyword.exception"] = { fg = c.azure_1 },
+		["@keyword.exception"] = { fg = c.azure_1, bold = true }, -- Bold for exception keywords (semantic emphasis)
 		["@keyword.function"] = { fg = c.indigo_2 },
-		["@keyword.import"] = { fg = c.mauve_4 },
+		["@keyword.import"] = { fg = c.mauve_4, italic = true }, -- Italic for import statements (semantic emphasis)
 		["@keyword.modifier"] = { fg = c.mauve_2 },
 		["@keyword.operator"] = { fg = c.azure_1 },
 		["@keyword.repeat"] = { fg = c.violet_1 },
-		["@keyword.return"] = { fg = c.azure_2 }, -- Warning amber - returns important
+		["@keyword.return"] = { fg = c.azure_2, bold = true }, -- Bold for return statements (semantic emphasis)
 		["@keyword.storage"] = { fg = c.azure_1 },
 
 		-- Labels
@@ -78,7 +78,8 @@ function M.setup(colors, opts)
 
 		-- Literals
 		["@markup"] = { fg = c.azure_1 },
-		["@markup.emphasis"] = { italic = true },
+		["@markup.emphasis"] = { italic = true }, -- Italic for emphasized text (semantic emphasis)
+		["@markup.strong"] = { bold = true }, -- Bold for strong text (semantic emphasis)
 		["@markup.environment"] = { fg = c.azure_1 },
 		["@markup.link"] = { fg = c.azure_1, underline = true },
 		["@markup.link.label"] = { fg = c.azure_1 },
@@ -86,7 +87,7 @@ function M.setup(colors, opts)
 		["@markup.list.checked"] = { fg = c.azure_1 },
 		["@markup.list.unchecked"] = { fg = c.azure_1 },
 		["@markup.math"] = { fg = c.azure_1 }, -- Turquoise cyan
-		["@markup.quote"] = { fg = c.azure_1, italic = true },
+		["@markup.quote"] = { fg = c.azure_1, italic = true }, -- Italic for quoted text (semantic emphasis)
 		["@markup.raw"] = { fg = c.azure_1 },
 		["@markup.underline"] = { underline = true },
 
@@ -95,7 +96,7 @@ function M.setup(colors, opts)
 
 		-- Modules - Enhanced for Module System
 		["@module"] = { fg = c.azure_2 },
-		["@module.builtin"] = { fg = c.azure_1, bold = true }, -- Base turquoise - Built-in modules
+		["@module.builtin"] = { fg = c.azure_1, bold = true }, -- Bold for built-in modules (semantic emphasis)
 
 		-- Namespace support
 		["@namespace"] = { fg = c.azure_1 },
@@ -109,7 +110,7 @@ function M.setup(colors, opts)
 
 		-- Parameters - Refined function signatures
 		["@parameter"] = { fg = c.stone_2 },
-		["@parameter.builtin"] = { fg = c.azure_1, italic = true },
+		["@parameter.builtin"] = { fg = c.azure_1, italic = true }, -- Italic for built-in parameters (semantic emphasis)
 		["@parameter.reference"] = { fg = c.azure_1 },
 
 		-- Properties - Sophisticated rose elegance
@@ -120,7 +121,7 @@ function M.setup(colors, opts)
 
 		-- Strings - Sophisticated emerald harmony
 		["@string"] = { fg = c.emerald_1 },
-		["@string.documentation"] = { fg = c.azure_1, italic = true },
+		["@string.documentation"] = { fg = c.azure_1, italic = true }, -- Italic for documentation strings (semantic emphasis)
 		["@string.escape"] = { fg = c.azure_1 },
 		["@string.regex"] = { fg = c.azure_1 }, -- Base lime - regex patterns vibrant
 		["@string.special"] = { fg = c.azure_1 },
@@ -132,13 +133,13 @@ function M.setup(colors, opts)
 
 		-- Types - Sophisticated amber hierarchy
 		["@type"] = { fg = c.indigo_2 },
-		["@type.builtin"] = { fg = c.azure_1, bold = true },
-		["@type.definition"] = { fg = c.azure_1, bold = true },
+		["@type.builtin"] = { fg = c.azure_1, bold = true }, -- Bold for built-in types (semantic emphasis)
+		["@type.definition"] = { fg = c.azure_1, bold = true }, -- Bold for type definitions (semantic emphasis)
 		["@type.qualifier"] = { fg = c.azure_1 },
 
 		-- Variables - Enhanced readability
 		["@variable"] = { fg = c.stone_1 },
-		["@variable.builtin"] = { fg = c.azure_1, italic = true },
+		["@variable.builtin"] = { fg = c.azure_1, italic = true }, -- Italic for built-in variables (semantic emphasis)
 		["@variable.member"] = { fg = "#cc909d" }, -- Base rose - members elegant
 		["@variable.parameter"] = { fg = c.stone_3 },
 
